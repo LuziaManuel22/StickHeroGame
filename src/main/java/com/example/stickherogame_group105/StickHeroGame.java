360be121a99f41eec5c65eb8458ed065d54f0717
M-Ria <108580450+M-Ria@users.noreply.github.com>
2023-12-07 19:44:37 +0530

    Added chnages to stick hero screen for basic view of the game

--- src/main/java/com/example/stickherogame_group105/StickHeroGame.java
+++ src/main/java/com/example/stickherogame_group105/StickHeroGame.java
@@ -7,8 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -19,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class SkeletonScreen extends Application {
+public class StickHeroGame extends Application {
 
     private static final double PLATFORM_WIDTH = 100.0;
     private static final double PLATFORM_HEIGHT = 10.0;
@@ -45,9 +43,6 @@ public class SkeletonScreen extends Application {
     private Rectangle platform;
     private double stickLength;
 
-    private List<Image> cherriesImages;
-    private List<ImageView> cherriesOnPlatform;
-
     private Color[] backgroundColors = {
             Color.LIGHTGRAY,
             Color.SKYBLUE,
@@ -57,7 +52,6 @@ public class SkeletonScreen extends Application {
     private int currentBackgroundColorIndex = 0;
 
     private boolean powerUpActive = false;
-    private List<ImageView> spikesOnPlatform;
 
     public static void main(String[] args) {
         launch(args);
@@ -65,15 +59,8 @@ public class SkeletonScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        initializeImages();
         createUI(primaryStage);
-    }
-
-    private void initializeImages() {
-        cherriesImages = new ArrayList<>();
-        cherriesImages.add(new Image("cherry1.png"));
-        cherriesImages.add(new Image("cherry2.png"));
-        cherriesImages.add(new Image("cherry3.png"));
+        animateBackgroundColorChange();
     }
 
     private void createUI(Stage primaryStage) {
@@ -82,7 +69,6 @@ public class SkeletonScreen extends Application {
         setupLabels();
         setupPowerUpButton();
         setupLayout(primaryStage);
-        animateBackgroundColorChange();
     }
 
     private void setupCanvas() {
@@ -96,13 +82,10 @@ public class SkeletonScreen extends Application {
         startButton.setOnAction(e -> startGame());
 
         restartButton = new Button("Restart");
-        restartButton.setOnAction(e -> restartGame());
+        restartButton.setOnAction(e -> resetGame());
 
         powerUpButton = new Button("Power Up!");
         powerUpButton.setOnAction(e -> activatePowerUp());
-    }
-
-    private void restartGame() {
     }
 
     private void setupLabels() {
@@ -120,14 +103,10 @@ public class SkeletonScreen extends Application {
     }
 
     private void activatePowerUp() {
-        if (!powerUpActive && cherries >= 5) {
+        if (!powerUpActive) {
             System.out.println("Power-Up Activated: Super Jump!");
             powerUpActive = true;
             jumpAnimation();
-            // Deduct 5 cherries for the power-up
-            cherries -= 5;
-            cherriesLabel.setText("Cherries: " + cherries);
-
             powerUpButton.setDisable(true);
             Timeline powerUpTimeline = new Timeline(
                     new KeyFrame(Duration.seconds(5),
@@ -191,7 +170,6 @@ public class SkeletonScreen extends Application {
         resetGame();
         generatePlatform();
         extendStick();
-        spawnCherryOnPlatform();
     }
 
     private void resetGame() {
@@ -203,8 +181,6 @@ public class SkeletonScreen extends Application {
         levelLabel.setText("Level: 1");
         stickProgressBar.setProgress(0);
         drawBackground();
-        cherriesOnPlatform = new ArrayList<>();
-        spikesOnPlatform = new ArrayList<>();
         powerUpActive = false;
         powerUpButton.setDisable(false);
     }
@@ -213,7 +189,6 @@ public class SkeletonScreen extends Application {
         stickLength = new Random().nextDouble() * 150 + 50;
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(1),
-                        new KeyValue(gameCanvas.translateXProperty(), gameCanvas.getTranslateX() + stickLength),
                         new KeyValue(scoreLabel.textProperty(), "Score: " + (++score)),
                         new KeyValue(stickProgressBar.progressProperty(), 1)
                 )
@@ -223,8 +198,6 @@ public class SkeletonScreen extends Application {
                 increaseLevel();
             }
             checkCollision();
-            // Collect cherries while extending the stick
-            collectCherries();
         });
         timeline.play();
     }
@@ -232,58 +205,16 @@ public class SkeletonScreen extends Application {
     private void generatePlatform() {
         double platformWidth = new Random().nextDouble() * 150 + 50;
         platform = new Rectangle(platformWidth, PLATFORM_HEIGHT, Color.GREEN);
-
-        // Add variations to platform appearance
-        if (new Random().nextBoolean()) {
-            // 50% chance to change platform color to brown
-            platform.setFill(Color.BROWN);
-        }
-
         platform.setTranslateX(gameCanvas.getTranslateX() + gameCanvas.getWidth());
         platform.setTranslateY(gameCanvas.getHeight() - PLATFORM_HEIGHT);
-        gc.setFill(platform.getFill());
+        gc.setFill(Color.GREEN);
         gc.fillRect(platform.getTranslateX(), platform.getTranslateY(), platformWidth, PLATFORM_HEIGHT);
-    }
-
-    private void spawnCherryOnPlatform() {
-        if (new Random().nextInt(4) == 0) {
-            // 25% chance to spawn a cherry
-            Image cherryImage = cherriesImages.get(new Random().nextInt(cherriesImages.size()));
-            ImageView cherry = new ImageView(cherryImage);
-            cherry.setFitWidth(HERO_WIDTH);
-            cherry.setFitHeight(HERO_HEIGHT);
-            cherry.setTranslateX(platform.getTranslateX() + platform.getWidth() / 2 - HERO_WIDTH / 2);
-            cherry.setTranslateY(platform.getTranslateY() - HERO_HEIGHT);
-            cherriesOnPlatform.add(cherry);
-            gc.drawImage(cherryImage, cherry.getTranslateX(), cherry.getTranslateY(), HERO_WIDTH, HERO_HEIGHT);
-        }
-    }
-
-    private void collectCherries() {
-        List<ImageView> collectedCherries = new ArrayList<>();
-        for (ImageView cherry : cherriesOnPlatform) {
-            // Check if the hero collects the cherry
-            double heroX = gameCanvas.getTranslateX() + stickLength;
-            double cherryX = cherry.getTranslateX();
-            if (heroX >= cherryX && heroX <= cherryX + HERO_WIDTH) {
-                collectedCherries.add(cherry);
-            }
-        }
-
-        // Remove collected cherries
-        cherriesOnPlatform.removeAll(collectedCherries);
-        cherries += collectedCherries.size();
-        cherriesLabel.setText("Cherries: " + cherries);
     }
 
     private void increaseLevel() {
         level++;
         cherries += level * 2;
         cherriesLabel.setText("Cherries: " + cherries);
-        cherriesOnPlatform.clear();
-        spikesOnPlatform.clear();
-        spawnCherryOnPlatform();
-        spawnSpikeOnPlatform();
         startButton.setDisable(true);
         Timeline levelUpTimeline = new Timeline(
                 new KeyFrame(Duration.seconds(2),
@@ -316,16 +247,8 @@ public class SkeletonScreen extends Application {
                 )
         );
         timeline.setOnFinished(e -> {
-            for (ImageView cherry : cherriesOnPlatform) {
-                cherry.setTranslateX(cherry.getTranslateX() - stickLength);
-            }
-            for (ImageView spike : spikesOnPlatform) {
-                spike.setTranslateX(spike.getTranslateX() - stickLength);
-            }
             generatePlatform();
             extendStick();
-            spawnCherryOnPlatform();
-            spawnSpikeOnPlatform();
         });
         timeline.play();
     }
@@ -341,19 +264,8 @@ public class SkeletonScreen extends Application {
         timeline.play();
     }
 
-    private void spawnSpikeOnPlatform() {
-        ImageView spike = new ImageView(new Image("spike.png"));
-        spike.setFitWidth(HERO_WIDTH);
-        spike.setFitHeight(HERO_HEIGHT);
-        spike.setTranslateX(platform.getTranslateX() + platform.getWidth() / 2 - HERO_WIDTH / 2);
-        spike.setTranslateY(platform.getTranslateY() - HERO_HEIGHT);
-        spikesOnPlatform.add(spike);
-        gc.drawImage(spike.getImage(), spike.getTranslateX(), spike.getTranslateY(), HERO_WIDTH, HERO_HEIGHT);
-    }
-
     private void drawBackground() {
         gc.setFill(Color.LIGHTGRAY);
         gc.fillRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
     }
 }
-
